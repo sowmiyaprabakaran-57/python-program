7079cfc1ffc5e1b44d5eb39a0d37c98308904e98
sowmiyaprabakaran-57 <168167244+sowmiyaprabakaran-57@users.noreply.github.com>
2024-07-22 23:16:11 +0530

    Create sort_colors.py

---
+++ 22.07.2024/sort_colors.py
@@ -0,0 +1,15 @@
+def sort_colors(nums):
+    low, mid, high = 0, 0, len(nums) - 1
+    while mid <= high:
+        if nums[mid] == 0:
+            nums[low], nums[mid] = nums[mid], nums[low]
+            low += 1
+            mid += 1
+        elif nums[mid] == 1:
+            mid += 1
+        else:
+            nums[high], nums[mid] = nums[mid], nums[high]
+            high -= 1
+arr = [2,0,2,1,1,0]
+sort_colors(arr)
+print(arr)  
